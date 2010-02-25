6e62cade5aeecbda67bd64e51b21ea0b3a15fbd9
inthejm <inthejm@5abed23c-1faa-11df-9e62-c93c6248c2ac>
2010-02-25 01:08:39 +0000

    LoackFreeQueue 추가요~

---
+++ ANE/Include/AneUtil/LockFreeStack.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace Mogi
+{
+
+}//namespace Mogi
--- ANE/Source/Test/Main.cpp
+++ ANE/Source/Test/Main.cpp
@@ -1,7 +1,16 @@
+#include "AneUtil.h"
 
-
+class Test
+{
+public:
+	int m_Num;
+};
 
 void main()
 {
+	AneUtil::LockFreeQueue<Test>			queTest;
+	Test* pTest = new Test;
+	Test temp ;
+	queTest.Push(temp);
 
 }
